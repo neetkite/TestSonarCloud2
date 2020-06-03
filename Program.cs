c957f2cc7ea3bc7a7c4b02987c7fe234e15319b0
neetkite <tienpvse130401@fpt.edu.vn>
2020-06-03 20:26:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSonarCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int16.Parse(Console.ReadLine());
+            int b = Int16.Parse(Console.ReadLine());
+            int result = Math(ref a,ref b);
+            Console.Write(result.ToString());
+            Console.ReadLine();
+
+        }
+
+        static int Math(ref int a, ref int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
